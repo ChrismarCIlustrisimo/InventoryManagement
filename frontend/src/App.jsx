47a1ec633f3bf462e6a6e69489d492152ddd6ebb
Chrismar Ilustrisimo <chrismarilustrisimo9@gmail.com>
2024-10-28 22:41:40 +0800

    sd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -103,11 +103,12 @@ const App = () => {
                                           <Route path="*" element={<Navigate to="/iRIG/" />} />
                                           <Route path="/unauthorized" element={<Unauthorized />} />
                                           <Route path="/iRIG/our-store" element={<StoreLocation />} />
-                                          <Route path="/iRIG/products/view/:id" element={<ViewProducts />} />
                                           <Route path="/iRIG/contact-us/" element={<ContactUs />} />
                                           <Route path="/iRIG/warranty/" element={<Warranty />} />
                                           <Route path="/iRIG/return-policy/" element={<ReturnPolicy />} />
                                           <Route path="/iRIG/faq/" element={<Faq />} />
+
+                                          <Route path="/iRIG/products/view/:id" element={<ViewProducts />} />
                                           <Route path="/iRIG/view-cart" element={<CartPage />} />
                                           <Route path="/iRIG/view-cart" element={<CartPage />} />
                                           <Route path="/Ereceipt" element={<Erceipt />} />
